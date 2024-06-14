b8260c2432daa8c5eb3c2250cea64d0bd73f99a8
GAURAV SHRIVASTAVA <gauravshrivastava890@gmail.com>
2024-06-14 14:54:04 +0530

    added some comment

--- logging_config.py
+++ logging_config.py
@@ -4,7 +4,7 @@ from config import Config
 
 def setup_logging():
     # Set up basic logging configuration
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO) #all INFO, WARNING, ERROR, and CRITICAL messages will be logged.
 
     # Skip file rotation during testing if TESTING attribute is defined
     if hasattr(Config, 'TESTING') and Config.TESTING:
@@ -17,11 +17,11 @@ def setup_logging():
     handler.setFormatter(formatter)
 
     # Add handler to the root logger
-    logger = logging.getLogger()
-    logger.addHandler(handler)
+    logger = logging.getLogger()    # Gets the root logger.
+    logger.addHandler(handler)      #Adds the rotating file handler to the root logger,
 
     # Log uncaught exceptions from Flask's WSGI server
     logging.getLogger('werkzeug').addHandler(handler)
     
     # Enable logging for SQLAlchemy queries
-    logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+    logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO) #enables logging of SQL queries executed by SQLAlchemy
